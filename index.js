02183ad15bc75b9599445a9f80021882e3dcb0f2
Rishav Kundu <rishav.kundu98@gmail.com>
2015-06-29 20:44:26 +0530

    minified stuff

--- index.js
+++ index.js
@@ -1,53 +1,5 @@
-(new Image()).src = path;
-
-
-$(document).ready(function()
-{
-	var check = function()
-	{
-		var l1 = $('#name').val()
-		var l2 = $('#msg').val()
-		if (l1 != "" && l2 != "")
-		{
-			return true;
-		}
-		else
-			return false;
-	};
-	var sanitise1 = function(e)
-	{
-
-		var keycode = e.keyCode;
-		if (keycode == 13)
-		{
-			$('button').click()
-		}
-		else if (65<=keycode && keycode <= 90)
-		{
-			return true;
-		}
-		else if (97<=keycode && keycode <= 122)
-		{
-			return true;
-		}
-		else return false;
-	};
-	$('input').bind('keypress',sanitise1);
-
-	$('.wrap').css('background-image',"url('"+path+"')");
-	$('button').click(function()
-	{
-		if (check())
-		{
-		var name = $('#name').val();
-		var msg = $('#msg').val();
-		$('.container.form').addClass('fade-up');
-		$('.container.form').remove();
-
-		$('.wrap').addClass('brighten');
-		$('#line1').html('Focus, '+name);
-		$('#line2').html(msg+' beckons.')
-		$('.container.message').addClass('fade-down');
-		}
-	});
-});
+(new Image()).src=path;$(document).ready(function(){var check=function(){var l1=$('#name').val()
+var l2=$('#msg').val()
+if(l1!=""&&l2!=""){return true;}else
+return false;};var sanitise1=function(e){var keycode=e.keyCode;if(keycode==13){$('button').click()}else if(65<=keycode&&keycode<=90){return true;}else if(97<=keycode&&keycode<=122){return true;}else return false;};$('input').bind('keypress',sanitise1);$('.wrap').css('background-image',"url('"+path+"')");$('button').click(function(){if(check()){var name=$('#name').val();var msg=$('#msg').val();$('.container.form').addClass('fade-up');$('.container.form').remove();$('.wrap').addClass('brighten');$('#line1').html('Focus, '+name);$('#line2').html(msg+' beckons.')
+$('.container.message').addClass('fade-down');}});});
